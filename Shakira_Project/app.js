3442bc83383ae42f8dd31576a01e713c017589da
Eudald <149712038+22Ebosch@users.noreply.github.com>
2024-03-05 15:32:51 +0100

    Conexion BBDD

---
+++ Shakira_Project/app.js
@@ -0,0 +1,25 @@
+//import express from 'express';
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 6666;
+const {Client} = require('pg'); 
+
+app.listen(PORT,()=>{
+    console.log('listening on port ' + PORT);
+});
+app.disable('x-powered-by');
+app.get('/', (req, res) => {
+    client.query('SELECT * FROM film where film_id = 1')
+    .then((result => res.json(result.rows)))
+    .catch(error => {
+        console.error('Error executing query', error);
+        res.status(500).send('Error executing query');
+    });
+});
+let dbConnection = fs.readFileSync('dbConnection.json');
+const client = new Client(JSON.parse(dbConnection));
+
+client.connect()
+    .then(() => console.log('Connected to PostgreSQL'))
+    .catch(err => console.error('Error connecting to PostgreSQL', err));
