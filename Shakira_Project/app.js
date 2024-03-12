1e170fd4fb53e3d08639ee1dac82218293505fca
juditperea <22jperea@ibadia.cat>
2024-03-12 17:11:09 +0100

    GET AND DELETE

--- Shakira_Project/app.js
+++ Shakira_Project/app.js
@@ -6,47 +6,53 @@ const PORT = 3334;
 const {Client} = require('pg'); 
 const path = require('path');
 
-// Configuración del motor de plantillas EJS
-app.use(express.static('bhumlu-lite')); // La carpeta donde se encuentran tus vistas
+
+app.use(express.static('bhumlu-lite')); 
 
 app.listen(PORT,()=>{
     console.log('listening on port ' + PORT);
 });
 app.disable('x-powered-by');
 
-app.get('/', (req, res) => {
-    client.query('SELECT * FROM film where film_id = 1')
-    .then((result => res.json(result.rows)))
-    .catch(error => {
-        console.error('Error executing query', error);
-        res.status(500).send('Error executing query');
-    });
-});
+//GET
 
 app.get('/home', (req, res) => {
-    res.sendFile(path.join(dirname + '/bhumlu-lite/index.html'));
-});
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(dirname, 'bhumlu-lite'));
-app.get('/staff', (req, res) => {
-    client.query('SELECT * FROM film')
-    .then(result => res.render('film', { staff: result.rows }))
-    .catch(error => {
-        console.error('Error ejecutando la consulta', error);
-        res.status(500).send('Error ejecutando la consulta');
-    });
+    res.sendFile(path.join(__dirname + '/bhumlu-lite/index.html'));
 });
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'bhumlu-lite'));
-app.get('/staff', (req, res) => {
-    client.query('SELECT * FROM staff')
-    .then(result => res.render('staff', { staff: result.rows }))
+app.get('/films', (req, res) => {
+    client.query('SELECT * FROM film')
+    .then(result => res.render('films', { film: result.rows }))
     .catch(error => {
         console.error('Error ejecutando la consulta', error);
         res.status(500).send('Error ejecutando la consulta');
     });
+});
+
+//DELETE
+
+app.get('/eliminar/:id', (req, res) => {
+    const { id } = req.params;
+    const query = 'DELETE FROM film_actor WHERE film_id = $1';
+    const query2 = 'DELETE FROM film_category WHERE film_id = $1';
+    const query3 = 'DELETE FROM inventory WHERE film_id = $1';
+    const query4 = 'DELETE FROM film WHERE film_id = $1';
+
+    client.query(query, [id])
+        .then()
+        .catch(err => console.error('Error ejecutando la consulta', err));
+
+        client.query(query2, [id])
+        .then()
+        .catch(err => console.error('Error ejecutando la consulta', err));
+        client.query(query3, [id])
+        .then()
+        .catch(err => console.error('Error ejecutando la consulta', err));
+        client.query(query4, [id])
+        .then(() => res.redirect('/films'))
+        .catch(err => console.error('Error ejecutando la consulta', err));
 });
 
 let dbConnection = fs.readFileSync('dbConnection.json');
