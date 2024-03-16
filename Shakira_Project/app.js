d05017109e42bf982dbc15f225e8ea94f5c8dfae
Eudald <149712038+22Ebosch@users.noreply.github.com>
2024-03-16 11:19:21 +0100

    eliminar funciona medio bien

--- Shakira_Project/app.js
+++ Shakira_Project/app.js
@@ -3,24 +3,24 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const PORT = 3334;
-const {Client} = require('pg'); 
+const { Client } = require('pg');
 const path = require('path');
 
 // Configuración del motor de plantillas EJS
 app.use(express.static('bhumlu-lite')); // La carpeta donde se encuentran tus vistas
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log('listening on port ' + PORT);
 });
 app.disable('x-powered-by');
 
 app.get('/', (req, res) => {
     client.query('SELECT * FROM film where film_id = 1')
-    .then((result => res.json(result.rows)))
-    .catch(error => {
-        console.error('Error executing query', error);
-        res.status(500).send('Error executing query');
-    });
+        .then((result => res.json(result.rows)))
+        .catch(error => {
+            console.error('Error executing query', error);
+            res.status(500).send('Error executing query');
+        });
 });
 
 app.get('/home', (req, res) => {
@@ -40,18 +40,29 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'bhumlu-lite'));
 app.get('/staff', (req, res) => {
     client.query('SELECT * FROM staff')
-    .then(result => res.render('staff', { staff: result.rows }))
-    .catch(error => {
-        console.error('Error ejecutando la consulta', error);
-        res.status(500).send('Error ejecutando la consulta');
-    });
+        .then(result => res.render('staff', { staff: result.rows }))
+        .catch(error => {
+            console.error('Error ejecutando la consulta', error);
+            res.status(500).send('Error ejecutando la consulta');
+        });
 });
 
 app.get('/eliminarStaff/:id', (req, res) => {
     const { id } = req.params;
-    const query = 'DELETE FROM staff WHERE staff_id = $1';
+    const query1 = 'SELECT * FROM rental WHERE staff_id = $1';
+    const query4 = 'DELETE FROM staff WHERE staff_id = $1';
 
-    client.query(query, [id])
-        .then(() => res.redirect('/staff'))
+    client.query(query1, [id])
+        .then(result => {
+            if(result.rows.length > 0){
+                res.json({message: 'No se puede eliminar porque tiene ventas registradas', status: 'error'});
+            } else {
+                client.query(query4, [id])
+                    .catch(err => console.error('Error ejecutando la consulta', err))
+                    .finally(() => {
+                        res.json({message: 'El miembro del personal ha sido eliminado correctamente', status: 'success'});
+                    });
+            }
+        })
         .catch(err => console.error('Error ejecutando la consulta', err));
 });
